cf9bc80f614dfae58838b35b348d91d7b946cfb1
Sahar Fayez <saharfayez2002@gmail.com>
2025-05-21 14:18:03 +0300

    change database schema

--- internal/app/database/migrations/1_create_table_users.up.sql
+++ internal/app/database/migrations/1_create_table_users.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "users"
+CREATE TABLE log.users
 (
     "id"       SERIAL PRIMARY KEY,
     "username" VARCHAR(50),
--- internal/app/database/migrations/2_create_table_posts.up.sql
+++ internal/app/database/migrations/2_create_table_posts.up.sql
@@ -1,4 +1,4 @@
-create table "posts"
+create table log.posts
 (
     "id"      SERIAL PRIMARY KEY,
     "title"   varchar(50),
